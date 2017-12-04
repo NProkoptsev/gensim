062c481c3bfa43919b2c03731ba78bfd00adea9f
Corvus <Corvu@users.noreply.github.com>
2017-12-05 01:05:33 +0300

    Change test dataset in test_paper2vec.py

--- gensim/test/test_paper2vec.py
+++ gensim/test/test_paper2vec.py
@@ -18,13 +18,22 @@ datapath_content = os.path.join(os.path.dirname(__file__), "test_data", "cora.co
 paper = namedtuple('paper', 'words tags')
 
 # Test dataset
-contents_test = [paper(words=['119', '126', '177', '253', '352', '457', '508', '522', '620', '649', '699', '703', '735', '846', '903', '1206', '1210', '1237', '1353', '1427'], tags=['31336']),
-    paper(words=['13', '510', '621', '764', '883', '894', '979', '1132', '1136', '1178', '1208', '1257', '1264', '1267', '1333', '1390', '1426'], tags=['1061127']),
-    paper(words=['46', '210', '213', '240', '293', '395', '511', '515', '582', '622', '624', '639', '1076', '1133', '1178', '1207', '1264', '1290', '1350', '1390', '1416', '1422'], tags=['1106406']),
-    paper(words=['42', '94', '100', '150', '595', '618', '625', '649', '875', '916', '943', '989', '1005', '1050', '1072', '1171', '1178', '1195', '1293', '1349', '1350'], tags=['13195']),
-    paper(words=['45', '123', '136', '154', '365', '397', '403', '475', '508', '620', '662', '700', '702', '829', '1067', '1175', '1176', '1178', '1209', '1210', '1213', '1255', '1382'], tags=['37879']),
-    paper(words=['94', '169', '212', '508', '527', '552', '875', '973', '1144', '1178', '1199', '1291', '1427'], tags=['1126012'])]
-citation_test = [(31336, 1126012), (31336, 1061127), (1061127, 13195), (1061127, 37879), (1106406, 1126012)]
+#contents_test = [paper(words=['119', '126', '177', '253', '352', '457', '508', '522', '620', '649', '699', '703', '735', '846', '903', '1206', '1210', '1237', '1353', '1427'], tags=['31336']),
+#    paper(words=['13', '510', '621', '764', '883', '894', '979', '1132', '1136', '1178', '1208', '1257', '1264', '1267', '1333', '1390', '1426'], tags=['1061127']),
+#    paper(words=['46', '210', '213', '240', '293', '395', '511', '515', '582', '622', '624', '639', '1076', '1133', '1178', '1207', '1264', '1290', '1350', '1390', '1416', '1422'], tags=['1106406']),
+#    paper(words=['42', '94', '100', '150', '595', '618', '625', '649', '875', '916', '943', '989', '1005', '1050', '1072', '1171', '1178', '1195', '1293', '1349', '1350'], tags=['13195']),
+#    paper(words=['45', '123', '136', '154', '365', '397', '403', '475', '508', '620', '662', '700', '702', '829', '1067', '1175', '1176', '1178', '1209', '1210', '1213', '1255', '1382'], tags=['37879']),
+#    paper(words=['94', '169', '212', '508', '527', '552', '875', '973', '1144', '1178', '1199', '1291', '1427'], tags=['1126012'])]
+#citation_test = [(31336, 1126012), (31336, 1061127), (1061127, 13195), (1061127, 37879), (1106406, 1126012)]
+
+# Test dataset
+contents_test = [paper(words=['machine', 'learning', 'deep', 'learning', 'neural', 'network', 'convolution'], tags=['1']),
+    paper(words=['neural', 'network', 'statistics', 'score', 'regression', 'backpropagation'], tags=['2']),
+    paper(words=['machine', 'learning', 'deep', 'learning', 'neural', 'network', 'convolution'], tags=['3']),
+    paper(words=['genetic', 'algorithms', 'epoch', 'score', 'changing', 'biological'], tags=['4']),
+    paper(words=['neural', 'network', 'backpropagation', 'feedforward', 'regression', 'overfitting', 'autoencoder'], tags=['5']),
+    paper(words=['reinforcement', 'learning', 'training', 'states', 'regression'], tags=['6'])]
+citation_test = [(1, 5), (1, 2), (2, 3), (3, 6), (4, 6)]
 
 # Test parameters
 d2v_params = {'alpha': 0.025, 'window': 10, 'min_count': 0, 'min_alpha': 0.025, 'size': 100}
@@ -48,21 +57,21 @@ class TestPaper2VecModel(unittest.TestCase):
     #    p2v.train()
 
     def test_train(self):
-        p2v = paper2vec.Paper2Vec(papers = contents_test, citation_graph = citation_test, 
+        p2v = paper2vec.Paper2Vec(papers = contents_test, citation_graph = citation_test,
             d2v_params = d2v_params, n2v_params = n2v_params)
         p2v.train()
-    
+
     def test_save_model_to_file(self):
         pass
 
     def test_get_item(self):
-        p2v = paper2vec.Paper2Vec(papers = contents_test, citation_graph = citation_test, 
+        p2v = paper2vec.Paper2Vec(papers = contents_test, citation_graph = citation_test,
             d2v_params = d2v_params, n2v_params = n2v_params)
         p2v.train()
         print(p2v['31336'])
 
     def test_get_similar(self):
-        p2v = paper2vec.Paper2Vec(papers = contents_test, citation_graph = citation_test, 
+        p2v = paper2vec.Paper2Vec(papers = contents_test, citation_graph = citation_test,
             d2v_params = d2v_params, n2v_params= n2v_params)
         p2v.train()
         print(p2v.pv['31336'])
